dee5970e2eeb9f353fd4823cba1d9aea6ebd5448
Emilie90 <105488500+Emilie90@users.noreply.github.com>
2023-03-01 14:21:11 +0100

    Delete backend directory

--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userCtrl = require("../controllers/user");
-const emailValidator = require("../middleware/emailValidator");
-router.post("/signup", emailValidator, userCtrl.signup);
-router.post("/login", userCtrl.login);
-
-module.exports = router;
